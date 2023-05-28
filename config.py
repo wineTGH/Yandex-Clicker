95a50bea1225a95f78ebf900570d645cbc18119c
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2023-05-28 20:06:44 +1000

    added code

---
+++ config.py
@@ -0,0 +1,9 @@
+search_term = "Купить постельное бельё"
+
+DEBUG = True
+
+link_masks = [
+    r"*",
+]
+
+index = 1
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import random
+import json
+import re
 
 from selenium import webdriver
 from selenium_stealth import stealth
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.common.by import By
 from random_user_agent.user_agent import UserAgent
 from random_user_agent.params import (
     OperatingSystem,
     HardwareType,
 )
 
-DEBUG = True
+import config as CONFIG
+
 
 class VirtualBrowser:
     driver: webdriver.Chrome
@@ -19,22 +24,22 @@ class VirtualBrowser:
         user_agent_rotator = UserAgent(
             operating_systems=operating_systems, hardware_types=hardware_types, limit=10
         )
-        
-        viewport_width = random.randint(300, 500)
-        viewport_height = random.randint(600, 900)
-        
+
+#        viewport_width = random.randint(300, 500)
+#        viewport_height = random.randint(600, 900)
+
         mobile_emulation = {
-            "deviceMetrics": {
-                "width": viewport_width,
-                "height": viewport_height,
-                "pixelRatio": 3.0,
-            },
+            # "deviceMetrics": {
+            #     "width": viewport_width,
+            #     "height": viewport_height,
+            #     "pixelRatio": 3.0,
+            # },
             "userAgent": user_agent_rotator.get_random_user_agent(),
         }
 
         options = webdriver.ChromeOptions()
 
-        if not DEBUG:
+        if not CONFIG.DEBUG:
             options.add_argument("--headless")
 
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -57,18 +62,46 @@ class VirtualBrowser:
 
     def find_advert(self, search_term: str):
         self.driver.get("https://ya.ru/")
-        search_box = self.driver.find_element(value='text')
-        search_box.send_keys(search_term)
-        search_box.submit()
-
         input()
-        cards_selector = "li.serp-item"
-        ad_cards_selector = "div.Organic>div.Organic-ContentWrapper>div.TextContainer>span[role=text]>span"
+        search_input = self.driver.find_element(value="text")
+        search_input.send_keys(search_term)
+        search_input.submit()
+
+        # Captcha xpath: /html/body/div[1]/div/div/form/div[3]
+        # Sound button xpath: /html/body/div[1]/div/div/form/div/div[3]/button[2]
+
+        search_cards = self.driver.find_element(By.ID, "search-result").find_elements(
+            By.TAG_NAME, "li"
+        )
+        ad_links: list[WebElement] = []
+        for search_card in search_cards:
+            try:
+                text_element = search_card.find_element(
+                    By.XPATH, ".//div/div[3]/div[1]/span/span[1]"
+                )
+                print(text_element.text[:-2])
+                if text_element.text[:-2] == "Реклама":
+                    ad_link = search_card.find_element(By.XPATH, ".//div/div[1]/a")
+
+                    json_data = ad_link.get_attribute("data-bem")
+                    json_data = json.loads(json_data)
+                    link = json_data.get("click").get("arguments").get("url")
+                    for link_mask in CONFIG.link_masks:
+                        if re.match(link_mask, link) is not None:
+                            ad_links.append(ad_link)
+                            break
+            except:
+                continue
+
+        ad_links[
+            max(-(len(ad_links) - 1), min(CONFIG.index - 1, len(ad_links) - 1))
+        ].click()
 
 
 def main():
     browser = VirtualBrowser()
-    browser.find_advert("Купить ноутбук")
+    browser.find_advert(CONFIG.search_term)
+
 
 if __name__ == "__main__":
     main()
