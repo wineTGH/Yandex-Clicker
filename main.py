4bd7dcce65e78e107b14ad380e90cf719caaec12
Anatoly Bogomolov <tolya.bogomolov2019@gmail.com>
2023-06-01 13:02:13 +1000

    refactoring

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import random
 import json
 import re
 import time
-from datetime import datetime
 
 from selenium import webdriver
 from selenium_stealth import stealth
@@ -16,20 +15,18 @@ from random_user_agent.params import (
     HardwareType,
 )
 
-
+from logger import Logger
 import config as CONFIG
 
 
-class Logger:
-    def __init__(self):
-        self.file = open(f"{datetime.today().strftime('%Y-%m-%d')}-logs.txt", "a")
-        print("Opened log file...")
-
-    def log(self, data, action: str):
-        self.file.write(f"{datetime.today().strftime('%Y-%m-%d %H:%M:%S')} --- {str(data)}  --- {action}\n")
-
 class VirtualBrowser:
     driver: webdriver.Chrome
+
+    xpath_dict = {
+        'search-result': '//*[@id="search-result"]',
+        'text-element': './/div/div[3]/div[1]/span/span[1]',
+        'ad-link-element': './/div/div[1]/a',
+    }
 
     def __init__(self):
         operating_systems = [OperatingSystem.ANDROID.value]
@@ -38,6 +35,14 @@ class VirtualBrowser:
             operating_systems=operating_systems, hardware_types=hardware_types, limit=10
         )
 
+        options = webdriver.ChromeOptions()
+
+        if not CONFIG.DEBUG:
+            options.add_argument("--headless")
+
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
+        
         if CONFIG.mobile:
             viewport_width = random.randint(300, 500)
             viewport_height = random.randint(600, 900)
@@ -50,16 +55,6 @@ class VirtualBrowser:
                 },
                 "userAgent": user_agent_rotator.get_random_user_agent(),
             }
-
-        options = webdriver.ChromeOptions()
-
-        if not CONFIG.DEBUG:
-            options.add_argument("--headless")
-
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
-        options.add_experimental_option("useAutomationExtension", False)
-        
-        if CONFIG.mobile:
             options.add_experimental_option("mobileEmulation", mobile_emulation)
 
         self.driver = webdriver.Chrome(
@@ -85,8 +80,8 @@ class VirtualBrowser:
 
         # Captcha xpath: /html/body/div[1]/div/div/form/div[3]
         # Sound button xpath: /html/body/div[1]/div/div/form/div/div[3]/button[2]
-
-        search_cards = self.driver.find_element(By.ID, "search-result").find_elements(
+        input()
+        search_cards = self.driver.find_element(By.XPATH, self.xpath_dict.get('search-result')).find_elements(
             By.TAG_NAME, "li"
         )
         ad_links: list[WebElement] = []
@@ -94,18 +89,18 @@ class VirtualBrowser:
         for search_card in search_cards:
             try:
                 text_element = search_card.find_element(
-                    By.XPATH, ".//div/div[3]/div[1]/span/span[1]"
+                    By.XPATH, self.xpath_dict.get('text-element')
                 )
+
                 print(text_element.text[:-2])
                 if text_element.text[:-2] == "Реклама":
-                    ad_link = search_card.find_element(By.XPATH, ".//div/div[1]/a")
+                    ad_link_element = search_card.find_element(By.XPATH, self.xpath_dict.get('ad-link-element'))
 
-                    json_data = ad_link.get_attribute("data-bem")
-                    json_data = json.loads(json_data)
-                    link = json_data.get("click").get("arguments").get("url")
+                    link = self.__get_url(ad_link_element)
+
                     for link_mask in CONFIG.link_masks:
                         if re.match(link_mask, link) is not None:
-                            ad_links.append(ad_link)
+                            ad_links.append(ad_link_element)
                             break
             except:
                 continue
@@ -130,6 +125,13 @@ class VirtualBrowser:
         
         finally:
             print("Page loaded")
+
+    def __get_url(self, element: WebElement):
+        json_data = element.get_attribute("data-bem")
+        json_data = json.loads(json_data)
+        link = json_data.get("click").get("arguments").get("url")
+
+        return link
 
 
 def main():
